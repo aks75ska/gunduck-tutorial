12bf86d086afd6a8d5b307ff409ddc06db5fc0c2
akshay goyal <aks75ska@gmail.com>
2016-06-06 16:44:13 +0530

    completed tut 3

--- gunduck/courses/views.py
+++ gunduck/courses/views.py
@@ -1,19 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from django.template import loader
 from courses.models import CourseTypes, Courses
 
 # Create your views here.
 def index(request):
     course_type_list = CourseTypes.objects.order_by('-created_time')[:10]
-    template = loader.get_template('courses/index.html')
     context = {
         'course_type_list': course_type_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'courses/index.html', context)
 
 def detail(request, course_type_id):
-    return HttpResponse("You're looking at course type %s." % course_type_id)
+    courseType = get_object_or_404(CourseTypes, pk=course_type_id)
+    return render(request, 'courses/detail.html', {'courseType': courseType})
 
 def results(request, course_type_id):
     response = "You're looking at the courses of the course type %s."
--- gunduck/gunduck/urls.py
+++ gunduck/gunduck/urls.py
@@ -13,6 +13,6 @@ from django.contrib import admin
 urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^courses/' , include('courses.urls')),
+    url(r'^courses/' , include('courses.urls', namespace="courses")),
 
 )
