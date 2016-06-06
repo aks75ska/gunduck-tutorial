68a26930b25b4a8561eec8a2ae64bfacb4191d9e
akshay goyal <aks75ska@gmail.com>
2016-06-06 16:18:22 +0530

    next step

--- gunduck/courses/views.py
+++ gunduck/courses/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from courses.models import CourseTypes, Courses
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the courses index.")
+    course_type_list = CourseTypes.objects.order_by('-created_time')[:10]
+    template = loader.get_template('courses/index.html')
+    context = {
+        'course_type_list': course_type_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, course_type_id):
     return HttpResponse("You're looking at course type %s." % course_type_id)
