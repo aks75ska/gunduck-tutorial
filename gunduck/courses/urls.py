6852bfe77525184847212573814739cd2252ca7c
akshay goyal <aks75ska@gmail.com>
2016-06-06 15:47:11 +0530

    adding more urls to courses

--- gunduck/courses/urls.py
+++ gunduck/courses/urls.py
@@ -6,6 +6,9 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 
-     url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<course_type_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<course_type_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<course_type_id>[0-9]+)/join/$', views.join, name='join'),
 
 )
--- gunduck/courses/views.py
+++ gunduck/courses/views.py
@@ -5,3 +5,13 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the courses index.")
 
+def detail(request, course_type_id):
+    return HttpResponse("You're looking at course type %s." % course_type_id)
+
+def results(request, course_type_id):
+    response = "You're looking at the courses of the course type %s."
+    return HttpResponse(response % course_type_id)
+
+def join(request, course_type_id):
+    return HttpResponse("You're joining a course of this course type %s." % course_type_id)
+
