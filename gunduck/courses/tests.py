35813d0f86ff12490f05ec37f3b290242cc98401
akshay goyal <aks75ska@gmail.com>
2016-06-09 17:42:26 +0530

    completed beginners tutorial

--- gunduck/courses/tests.py
+++ gunduck/courses/tests.py
@@ -3,10 +3,7 @@ from django.test import TestCase
 # Create your tests here.
 
 import datetime
-
 from django.utils import timezone
-from django.test import TestCase
-
 from .models import CourseTypes
 
 
