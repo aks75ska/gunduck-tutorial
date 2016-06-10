4f52a53f889d027230f9edc37f603e26b651f30d
akshay goyal <aks75ska@gmail.com>
2016-06-10 13:37:12 +0530

    webhook correction

--- gunduck/courses/views.py
+++ gunduck/courses/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponse
 from courses.models import CourseTypes, Courses
 from django.core.urlresolvers import reverse
+import json
 
 # Create your views here.
 def index(request):
