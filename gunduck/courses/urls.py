109684a40f8a22b4e63d4e42ee600a663098cfd6
akshay goyal <aks75ska@gmail.com>
2016-06-16 13:11:18 +0530

    small but important changes

--- gunduck/courses/urls.py
+++ gunduck/courses/urls.py
@@ -6,10 +6,10 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<course_type_id>[0-9]+)/$', views.detail, name='detail'),
-    url(r'^(?P<course_type_id>[0-9]+)/results/$', views.results, name='results'),
-    url(r'^(?P<course_type_id>[0-9]+)/join/$', views.join, name='join'),
+    # url(r'^$', views.index, name='index'),
+    # url(r'^(?P<course_type_id>[0-9]+)/$', views.detail, name='detail'),
+    # url(r'^(?P<course_type_id>[0-9]+)/results/$', views.results, name='results'),
+    # url(r'^(?P<course_type_id>[0-9]+)/join/$', views.join, name='join'),
     url(r'^chatbots/facebook/webhook/$', views.faceBookChatBot, name='faceBookChatBot'),
 
 )
